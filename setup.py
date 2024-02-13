ddbaa780633eb1e3018b55c264d9adf893d5f2a5
choudharyharsh122 <choudharyharsh122@gmail.com>
2024-02-13 17:01:11 +0100

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='your_package_name',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'ot',
+        'numpy',
+        'scipy',
+        'qpsolvers',
+        'autoray'
+    ],
+)
