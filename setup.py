411aeeacbc6e07e59398c55a35b8acfa10e9ea42
choudharyharsh122 <choudharyharsh122@gmail.com>
2024-04-09 10:18:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,6 @@ from setuptools import setup, find_packages
 from setuptools.extension import Extension
 from Cython.Build import cythonize
 
-# Define extension modules
-extensions = [
-    Extension('ghost.module1', ['your_library/module1.pyx']),
-    Extension('ghost.module2', ['your_library/module2.cpp'],
-              extra_compile_args=['-std=c++11']),
-]
-
-# Cythonize .pyx files
-extensions = cythonize(extensions)
-
 setup(
     name='StochasticGhost',
     version='0.1.0',
