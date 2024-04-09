ce6be26f5b8e09bd2a5a3f265071be306ac39fcc
choudharyharsh122 <choudharyharsh122@gmail.com>
2024-04-09 09:50:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,38 @@
+import os
 from setuptools import setup, find_packages
+from setuptools.extension import Extension
+from Cython.Build import cythonize
+
+# Define extension modules
+extensions = [
+    Extension('ghost.module1', ['your_library/module1.pyx']),
+    Extension('ghost.module2', ['your_library/module2.cpp'],
+              extra_compile_args=['-std=c++11']),
+]
+
+# Cythonize .pyx files
+extensions = cythonize(extensions)
 
 setup(
-    name='your_package_name',
-    version='0.1',
+    name='StochasticGhost',
+    version='0.1.0',
+    description='Empirical Risk minimization with constraints',
+    author='Harsh Choudhary',
+    author_email='choudharyharsh122@gmail.com',
     packages=find_packages(),
+    ext_modules=extensions,
+    install_requires=[python_requires='>=3.10',
     install_requires=[
-        'ot',
         'numpy',
+        'ot',
+        'warnings',
+        'argparse',
+        'time',
         'scipy',
         'qpsolvers',
-        'autoray'
+        'scikit-learn',
+        'matplotlib',
+        'autoray',
+    ]
     ],
 )
